753f687f3ea146d4cfbd1338c5125c57b1aaaf8d
Jhonne <jonee4339@hotmail.com>
2025-12-25 07:35:09 -0300

    finished create activities and get activities route

---
+++ src/routes/create-activity.ts
@@ -0,0 +1,52 @@
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma.ts";
+import { dayjs } from "../lib/dayjs.ts";
+
+export async function createActivity(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips/:tripId/activities",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          title: z.string().min(4),
+          occurs_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+      const { title, occurs_at } = request.body;
+
+      const trip = await prisma.trip.findUnique({
+        where: { id: tripId },
+      });
+
+      if (!trip) {
+        throw new Error("trip not found");
+      }
+
+      if (dayjs(occurs_at).isBefore(trip.starts_at)) {
+        throw new Error("Invalid Activity date");
+      }
+
+      if (dayjs(occurs_at).isAfter(trip.ends_at)) {
+        throw new Error("Invalid Activity date");
+      }
+
+      const activity = await prisma.activity.create({
+        data: {
+          title,
+          occurs_at,
+          trip_id: tripId,
+        },
+      });
+        
+      return { activityId: activity.id };
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip.ts";
 import { confirmParticipant } from "./routes/confirm-participant.ts";
+import { createActivity } from "./routes/create-activity.ts";
+import { getActivity } from "./routes/get-activities.ts";
 
 const app = fastify();
 
@@ -20,6 +22,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivity);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server Running");
